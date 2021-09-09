2813719f99baa7f2b2ef926c808bab6d38f43c90
Hunter <sz2hong@uwaterloo.ca>
2021-09-09 14:49:15 -0700

    encryptor

--- RSA.py
+++ RSA.py
@@ -1,5 +1,8 @@
 from key_generator import Key_Generator
+from encryptor import Encryptor
 
 if __name__ == '__main__':
-    Generator = Key_Generator(8, prime_1 = 233, prime_2 = 211, public_exponent = 20771)
-    Generator.get_key()
+    RSA_Generator = Key_Generator(8, prime_1 = 233, prime_2 = 211, public_exponent = 20771)
+    pub_key = RSA_Generator.get_key()
+    RSA_Encryptor = Encryptor(pub_key, 123)
+    print(RSA_Encryptor.encrypt())
--- encryptor.py
+++ encryptor.py
@@ -4,12 +4,31 @@ class Encryptor:
         self.public_exponent = public_key[1]
         self.message = message
     
-    def modular_exponent(num, mod, power):
-        exponent_2 = 0
-        power_2 = 1
-        power_num = 1
-        while power_2 <= power:
-            power_2 *=2
-            exponent_2 +=1
+    #assumes num < mod
+    #exponentiation by squaring..
+    def modular_exponent(self, num, power, mod):
+        if power == 0:
+            return 1
+        else:
+            power_2_lst = [1]
+            power_num_lst = [num]
+            while power_2_lst[-1] < power:
+                power_2_lst.append(power_2_lst[-1]*2)
+                power_num_lst.append((power_num_lst[-1] * power_num_lst[-1]) % mod )
+            exp = 1
+            #x^{2^{a_n} + ... + 2^{a_0}} = x^{2^{a_n}}*...*x^{2^{a_0}}
+            #we keep track of all x^{2^k}s in power_num_lst
+            while power > 0:
+                if power_2_lst[-1] <= power:
+                    power -= power_2_lst[-1]
+                    exp *= power_num_lst[-1]
+                    exp %= mod
+                power_2_lst = power_2_lst[:-1]
+                power_num_lst = power_num_lst[:-1]
+            return exp
+
+                
+    def encrypt(self):
+        return self.modular_exponent(self.message, self.public_exponent, self.public_modulus)
         
         
